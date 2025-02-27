bcf2f01bd89a1b9af127f1f2504927f2997fb675
wesleybentura13 <wesleybentura@gmail.com>
2025-02-27 11:20:43 +0200

    SQL injection

--- Code/routes/search.js
+++ Code/routes/search.js
@@ -11,9 +11,7 @@ module.exports = function searchProducts () {
   return (req, res, next) => {
     let criteria = req.query.q === 'undefined' ? '' : req.query.q || ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query('SELECT * FROM Products WHERE ((name LIKE? OR description LIKE?) AND deletedAt IS NULL) ORDER BY name', {
-      replacements: ['%' + criteria + '%']
-    })
+    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
       .then(([products]) => {
         const dataString = JSON.stringify(products)
         if (utils.notSolved(challenges.unionSqlInjectionChallenge)) {
